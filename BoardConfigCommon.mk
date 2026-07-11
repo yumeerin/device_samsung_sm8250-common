c3afbc7ac9bd2013fd5df5f8f3e3dae4a19aba0f
Yumeerin <chan.misaki.y@gmail.com>
2026-07-11 17:46:43 +0000

    sm8250: replace to circle

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -105,7 +105,7 @@ TARGET_USERIMAGES_USE_F2FS           := true
 TARGET_USERIMAGES_USE_EXT4           := true
 
 # Partitions
--include vendor/halcyon/config/BoardConfigReservedSize.mk
+-include vendor/lineage/config/BoardConfigReservedSize.mk
 
 BOARD_QTI_DYNAMIC_PARTITIONS_PARTITION_LIST := system system_ext odm product vendor
 BOARD_QTI_DYNAMIC_PARTITIONS_SIZE := $(shell echo $$(($(BOARD_SUPER_PARTITION_SIZE) - 4194304))) # (BOARD_SUPER_PARTITION_SIZE - "reasonable overhead of 4 MiB")
